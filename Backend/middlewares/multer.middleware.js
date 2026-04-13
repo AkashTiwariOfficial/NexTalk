067500c276e8a3992c62e35697097b76d7c14d88
AKashTiwariOfficial <akashtiwari00624@gmail.com>
2026-04-14 04:21:32 +0530

    completed the project setup

---
+++ Backend/middlewares/multer.middleware.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+
+    fileName(req, file, cb) {
+        const ext = path.extname(file.originalname);
+        const base = path.basename(file.originalname, ext);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        const safeBase = base.replace(/\s+/g, "_")
+        cb(null, `${safeBase}-${uniqueSuffix}${ext}`)
+    }
+
+})
+
+export const uplaod = multer({
+    storage
+}); 
