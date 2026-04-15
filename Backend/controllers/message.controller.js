03b0e99d658b0e12d406294e2f2523a2bf1910b9
AKashTiwariOfficial <akashtiwari00624@gmail.com>
2026-04-15 16:09:28 +0530

    completed message controller

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -1,8 +1,33 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Conversation } from '../models/conversation.model.js'
+import { Message } from "../models/message.model.js";
+import ApiResponses from "../utils/ApiResponses.js";
 
 const sendMessage = asyncHandler( async ( req, res )=>{
     const { id: recieverId } = req.params;
     const { message } = req.body;
     const senderId = req.user?._id;
-    
+
+     let conversation;
+
+     conversation = await Conversation.findOne({
+        participants: { $all: [ senderId, recieverId ]}
+    })
+
+    if(!conversation){
+          conversation = await Conversation.create({
+            participants: [ senderId, recieverId ]
+         })
+    }
+
+    const newMessage = new Message({ message, senderId, recieverId });
+    await conversation.messages.push(message);
+
+    const sentMessage = new Promise.all([
+        Conversation.save(),
+        Message.save()
+    ])
+
+    return res.status(200)
+    .json(new ApiResponses(200, sentMessage, "message sent successfully"));
 })
